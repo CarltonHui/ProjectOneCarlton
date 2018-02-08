524f897ef0233baa8f19fe00b54cef93fc5c22d8
Carlton Hui <carltonhui@gmail.com>
2018-02-07 16:40:23 -1000

    Coding Challenge #1 Completed

--- ProjectOne.playground/Contents.swift
+++ ProjectOne.playground/Contents.swift
@@ -3,3 +3,18 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//Variable Coding Challenge #1
+
+var firstString: String
+firstString = "I spend my free time building computers"
+
+let secondString = " and it is stressful but very rewarding!"
+
+var thirdString = firstString + secondString
+
+print (thirdString)
+
+//Variable Coding Challenge #2
+
+
