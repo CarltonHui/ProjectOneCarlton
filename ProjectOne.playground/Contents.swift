e7d7e2fe8e4d9390be99e557167d452e61d4fe4d
Carlton Hui <carltonhui@gmail.com>
2018-02-07 19:40:07 -1000

    challenge #2 completed

--- ProjectOne.playground/Contents.swift
+++ ProjectOne.playground/Contents.swift
@@ -8,13 +8,17 @@ var str = "Hello, playground"
 
 var firstString: String
 firstString = "I spend my free time building computers"
-
 let secondString = " and it is stressful but very rewarding!"
-
 var thirdString = firstString + secondString
-
 print (thirdString)
 
 //Variable Coding Challenge #2
+
+var celcius: Float
+var fahrenheit: Float = 50
+celcius = (5/9)*(fahrenheit-32)
+print ("Today it is \(celcius) degrees celcius")
+
+//Boolean Variables Coding Challenge #1
 
 
